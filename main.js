3184ecf25581dc88f103d5d5e9a9c0540bb3e2c4
Louis Atkinson <119425566+LouisAtkinson@users.noreply.github.com>
2023-02-05 13:50:15 +0000

    Add new blank html pages and begin about me

--- main.js
+++ main.js
@@ -1,7 +1,27 @@
 const intro = document.getElementById('intro');
 const info = document.getElementById('info');
+const header = document.getElementById('header');
+const about = document.getElementById('aboutlink');
+const projects = document.getElementById('projectslink');
+const contact = document.getElementById('contactlink');
 
 function homeAnimations() {
     setTimeout(function(){intro.style.color = 'white';},1000);
     setTimeout(function(){info.style.color = 'white';},2000);
 }
+
+header.addEventListener('click', function() {
+    window.location.href="index.html";
+})
+
+about.addEventListener('click', function() {
+    window.location.href="about.html";
+})
+
+projects.addEventListener('click', function() {
+    window.location.href="projects.html";
+})
+
+contact.addEventListener('click', function() {
+    window.location.href="contact.html";
+})
