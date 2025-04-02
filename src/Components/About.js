4fde93bf5e83c6430a4a8c05197ed2ba3da2fad2
Louis Atkinson <louisatkinson.la@gmail.com>
2025-04-02 20:26:39 +0100

    update CV, portfolio and about, up to 1.0.0

--- src/Components/About.js
+++ src/Components/About.js
@@ -9,29 +9,37 @@ const About = () => {
         <div className={`about-text ${!showCV ? 'active' : ''}`}>
           <h2>ABOUT</h2>
           <p className="fade-in">
-            Hello! I’m Louis Atkinson, a Sheffield-based software developer with a passion for solving complex problems and learning new things. 
-            In my current role, I specialise in front-end development, particularly in game development, 
-            where I take pride in creating visually engaging projects that provide an excellent user experience. 
-            Some of these can be seen in my portfolio.
+          Hello! I’m Louis Atkinson, a Sheffield-based software developer with a passion for solving complex problems and learning new technologies. 
+          In my current role, I specialise in full-stack development, with experience in game development using Java and JavaScript. 
+          I take pride in creating efficient, well-designed systems that deliver a great user experience. Some of my work can be seen in my portfolio.
           </p>
-          <p className="fade-in">
-            While my professional work currently focuses on the front-end, I’m equally comfortable across the entire stack. 
-            In my free time, I enjoy building full-stack applications, 
-            which allows me to sharpen both my front-end and back-end skills and learn new technologies. 
-            This enables me to tackle a wide range of challenges, from crafting intuitive interfaces to implementing efficient back-end logic, 
-            ensuring everything works seamlessly.
-          </p>
-          {false && (
-            <p className="fade-in">
-              I’m driven by the opportunity to grow as a developer, always seeking to improve and tackle new challenges. 
-              For example, I’m currently working towards the AWS Solutions Architect Associate certification and 
-              applying these skills in the development of a chess website.
-            </p>
-          )}
-          <p>
-            If you’re looking for a developer who’s adaptable, eager to learn, and proficient across both the front-end and back-end, 
-            feel free to explore my portfolio or get in touch.
-          </p>
+          {
+        //     <p className="fade-in">
+        //   While my professional work currently focuses on the front-end, I’m equally comfortable across the entire stack. 
+        //   In my free time, I enjoy building full-stack applications, 
+        //   which allows me to sharpen both my front-end and back-end skills and learn new technologies. 
+        //   This enables me to tackle a wide range of challenges, from crafting intuitive interfaces to implementing efficient back-end logic, 
+        //   ensuring everything works seamlessly.
+        // </p>
+
+          }
+          
+          {
+          //   false && (
+          //   <p className="fade-in">
+          //     I’m driven by the opportunity to grow as a developer, always seeking to improve and tackle new challenges. 
+          //     For example, I’m currently working towards the AWS Solutions Architect Associate certification and 
+          //     applying these skills in the development of a chess website.
+          //   </p>
+          // )
+        }
+          {
+            //   <p>
+            //   If you’re looking for a developer who’s adaptable, eager to learn, and proficient across both the front-end and back-end, 
+            //   feel free to explore my portfolio or get in touch.
+            // </p>
+          }
+
         </div>
 
         {showCV && (
--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -4,30 +4,29 @@ const Portfolio = () => {
   const projects = [
     {
       title: 'Monopoly Super Wheel Bonus',
-      description: 'An online slot machine game I built the front-end for using the PixiJS and GSAP libraries.',
+      description: 'An online slot machine game built using the PixiJS and GSAP libraries. It was a complicated project, featuring multiple (sometimes simultaneous) bonuses, including cascades, expanding reels, and a pick game.',
       imageUrl: 'images/monopoly-super-wheel-bonus.jpg',
       projectUrl: 'https://slotslaunch.com/light-and-wonder/monopoly-super-wheel-bonus'
     },
     {
       title: 'Yukon Gold',
-      description: 'Another online slot machine game I built the front-end for using the PixiJS and GSAP libraries.',
+      description: 'An online slot machine game built using the PixiJS and GSAP libraries. Via the \'BUY PASS\', the hold\'n\'spin bonus can be seen, which combines masks, filters, animated sprites and Spine animations for a rich visual experience.',
       imageUrl: 'images/yukon-gold.jpg',
       projectUrl: 'https://slotslaunch.com/crucible-gaming/yukon-gold'
     },
     {
+      title: 'Monster Catch',
+      description: 'A slot machine game originally developed for casino cabinets, which I ported to web and mobile platforms. The performance was optimised to ensure smooth gameplay while preserving the high-quality visuals of the original.',
+      imageUrl: 'images/monster-catch.jpg',
+      projectUrl: 'https://slotslaunch.com/light-and-wonder/monster-catch'
+    },
+    {
       title: 'Socialise',
-      description: 'A Facebook-inspired social media site built using React.js and Typescript on the front-end, and Express.js and MongoDB on the back-end.',
+      description: 'A Facebook-inspired social media site built using React.js and Typescript on the front-end, and Express.js and MongoDB on the back-end. Users can sign up, customise their profiles, connect and interact with friends, create posts, and upload profile pictures. Authentication is secured using JSON Web Tokens (JWT).',
       imageUrl: 'images/socialise.jpg',
       projectUrl: 'https://socialise-lz3f.vercel.app',
       githubUrl: 'https://github.com/LouisAtkinson/socialise',
-    },
-    {
-      title: 'React store',
-      description: 'A modern web store built using React.js.',
-      imageUrl: 'images/react-store.jpg',
-      projectUrl: 'https://louisatkinson.co.uk/react-store',
-      githubUrl: 'https://github.com/LouisAtkinson/react-store',
-    },
+    }
   ];
 
   return (
